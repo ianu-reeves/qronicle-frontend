55c7565e7db144e79ac063fb1863cdf94da983bb
Ian Campbell <campbelli23@mymacewan.ca>
2025-01-23 19:06:17 -0700

    - update axios to use remote backend IP address

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'http://52.42.122.118:8080/QRonicle-1.4-DEPLOY/';
 
 export default axios.create({
   baseURL: BASE_URL,
