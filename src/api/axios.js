ecd1b2f4805291bd429e77292bc22d35b76a3813
Ian Campbell <campbelli23@mymacewan.ca>
2025-01-24 16:54:36 -0700

    - update axios url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://18.237.89.92:8443//QRonicle-1.4-DEPLOY/';
+const BASE_URL = 'https://18.237.89.92:8443/QRonicle-1.4-DEPLOY/';
 
 export default axios.create({
   baseURL: BASE_URL,
