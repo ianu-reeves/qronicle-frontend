21f8fe08d7456eecf02fd05da9766edd7aef7394
Ian Campbell <campbelli23@mymacewan.ca>
2025-02-27 12:34:43 -0700

    - adjust settings for remote deployment

--- src/components/LinkedLogoButton.jsx
+++ src/components/LinkedLogoButton.jsx
@@ -1,14 +1,13 @@
 import {Avatar, Button} from "@mui/material";
 import React from "react";
 
-export default function LinkedLogoButton({ avatarSize, src, href, buttonText, onClick }) {
+export default function LinkedLogoButton({ avatarSize, src, buttonText, ...props }) {
   return (
     <Button
       variant="outlined"
       startIcon={<Avatar sx={{ width: avatarSize, height: avatarSize }} src={src} />}
       component="a"
-      href={href}
-      onClick={onClick}
+      {...props}
     >
       {buttonText}
     </Button>
